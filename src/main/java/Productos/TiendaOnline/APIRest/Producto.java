7eae1d0e563779d617cee44cb87a37ab8237a2aa
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-12 20:05:57 -0600

    Creando entidad Productos

---
+++ src/main/java/Productos/TiendaOnline/APIRest/Producto.java
@@ -0,0 +1,9 @@
+package Productos.TiendaOnline.APIRest;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private String marca;
+    private String descripcion;
+    private double precio;
+}
