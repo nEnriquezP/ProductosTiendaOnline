a90b0cae09974f37f0cf6b7b8b6c8975a6fcf32b
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-12 20:04:17 -0600

    Subiendo Proyecto

---
+++ src/main/java/Productos/TiendaOnline/APIRest/ProductosApplication.java
@@ -0,0 +1,13 @@
+package Productos.TiendaOnline.APIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Autor Nelson Enriquez
+@SpringBootApplication
+public class ProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosApplication.class, args);
+	}
+
+}
